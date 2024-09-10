6fa20bf4d5e4efe9efbf437c1a72245bb22b1d7f
ansenya <ars.anoshin@gmail.com>
2024-09-10 12:32:21 +0300

    get max

--- cl_app.py
+++ cl_app.py
@@ -1,56 +1,3 @@
-# class Song:
-#     def __init__(self, id, artist, Url_spotify, Track, Album, Album_type, Uri, Danceability, Energy, Key, Loudness, Speechiness, Acousticness, Instrumentalness, Liveness, Valence, Tempo, Duration_ms, Url_youtube, Title, Channel, Views, Likes, Comments, Description, Licensed, official_video, Stream):
-#         self.id = id
-#         self.artist = artist
-#         self.Url_spotify = Url_spotify
-#         self.Track = Track
-#         self.Album = Album
-#         self.Album_type = Album_type
-#         self.Uri = Uri
-#         self.Danceability = Danceability
-#         self.Energy = Energy
-#         self.Key = Key
-#         self.Loudness = Loudness
-#         self.Speechiness = Speechiness
-#         self.Acousticness = Acousticness
-#         self.Instrumentalness = Instrumentalness
-#         self.Liveness = Liveness
-#         self.Valence = Valence
-#         self.Tempo = Tempo
-#         self.Duration_ms = Duration_ms
-#         self.Url_youtube = Url_youtube
-#         self.Title = Title
-#         self.Channel = Channel
-#         self.Views = Views
-#         self.Likes = Likes
-#         self.Comments = Comments
-#         self.Description = Description
-#         self.Licensed = Licensed
-#         self.official_video = official_video
-#         self.Stream = Stream
-#         self.Danceability = Danceability
-#         self.Energy = Energy
-#         self.Key = Key
-#         self.Loudness = Loudness
-#         self.Speechiness = Speechiness
-#         self.Acousticness = Acousticness
-#         self.Instrumentalness = Instrumentalness
-#         self.Liveness = Liveness
-#         self.Valence = Valence
-#         self.Tempo = Tempo
-#         self.Duration_ms = Duration_ms
-#         self.Url_youtube = Url_youtube
-#         self.Title = Title
-#         self.Channel = Channel
-#         self.Views = Views
-#         self.Likes = Likes
-#         self.Comments = Comments
-#         self.Description = Description
-#         self.Licensed = Licensed
-#         self.official_video = official_video
-#         self.Stream = Stream
-#
-
 songs = []
 LAST_INDEX = 24
 
@@ -65,12 +12,22 @@ def read_data(path: str = 'data/Spotify_Youtube.csv'):
                 songs.append(line)
     return songs, header
 
+
 def get_shape(songs: []):
     return len(songs), len(songs[0])
 
+
+def get_max(songs, column):
+    m = 0
+    if songs[0][column].isdigit():
+        for i in range(len(songs)):
+            if float(songs[i][column]) > m:
+                m = float(songs[i][column])
+        return m
+    return -1
+
+
 if __name__ == "__main__":
     songs, header = read_data()
+    print("Max:", get_max(songs, 0))
     print(get_shape(songs))
-    # for song in songs:
-    #     print(song)
-    # print(len(songs))
