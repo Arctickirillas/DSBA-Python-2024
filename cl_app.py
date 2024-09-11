bf4845c2c37155f5cf6a5fb0ed413fb0f7cab35c
ansenya <ars.anoshin@gmail.com>
2024-09-11 14:22:15 +0300

    min_max average_likes argparse

--- cl_app.py
+++ cl_app.py
@@ -1,15 +1,13 @@
+import csv
+import argparse
+
 songs = []
 LAST_INDEX = 24
 
 def read_data(path: str = 'data/Spotify_Youtube.csv'):
-    songs = []
-
     with open(path, 'r') as file:
         header = file.readline().strip().split(',')
-        for line in file:
-            line = line.strip().split(',')
-            if len(line) > LAST_INDEX and line[0].isdigit():
-                songs.append(line)
+        songs = [element for element in csv.reader(file)]
     return songs, header
 
 
@@ -17,17 +15,35 @@ def get_shape(songs: []):
     return len(songs), len(songs[0])
 
 
-def get_max(songs, column):
-    m = 0
-    if songs[0][column].isdigit():
-        for i in range(len(songs)):
-            if float(songs[i][column]) > m:
-                m = float(songs[i][column])
-        return m
-    return -1
+def get_min_max(songs: [], column: int):
+    if column < 0 or column > len(songs[0]):
+        raise IndexError('out of range')
+    if songs[0][column].replace('.','', 1).isnumeric():
+        return (min([float(songs[i][column]) for i in range(len(songs)) if songs[i][column] != '']),
+                max([float(songs[i][column]) for i in range(len(songs)) if songs[i][column] != '']))
+    else:
+        raise TypeError('column must be a number')
+
+
+def get_average_likes(songs: []):
+    song_map = dict()
+    likes_map = dict()
+    for element in songs:
+        song_map[element[1]] = song_map.get(element[1], 0) + 1
+        likes_map[element[1]] = likes_map.get(element[1], 0) + float(element[23]) if element[23] else 0
+    for i in song_map:
+        song_map[i] =  likes_map[i] / song_map[i]
+    return sorted(song_map.items(), key=lambda x: x[1], reverse=True)
 
 
 if __name__ == "__main__":
-    songs, header = read_data()
-    print("Max:", get_max(songs, 0))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file')
+    args = parser.parse_args()
+
+    songs, header = read_data(args.file)
+    # print(songs)
+    # print(header)
+    print(get_min_max(songs, 9))
     print(get_shape(songs))
+    print(get_average_likes(songs))
