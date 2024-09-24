288a41b26e7ee79881522de671c2affdcb86c917
ansenya <ars.anoshin@gmail.com>
2024-09-24 12:29:42 +0300

    class pandas search

--- cl_app.py
+++ cl_app.py
@@ -1,19 +1,55 @@
-import csv
-import argparse
+from colorsys import yiq_to_rgb
+from xxsubtype import bench
 
+import pandas as pd
+
+class Song:
+    def __init__(self, row: pd.Series):
+        self.title = row.Title
+        self.artist = row.Artist
+        self.likes = row.Likes
+
+        self.Danceability = row.Danceability
+        self.Energy = row.Energy
+        self.Loudness = row.Loudness
+        self.Speechiness = row.Speechiness
+        self.Acousticness = row.Acousticness
+        self.Instrumentalness = row.Instrumentalness
+
+    def __repr__(self):
+        return f"|{self.title} by {self.artist} with {self.likes} likes|"
+
+class Database:
+    def __init__(self, path: str = 'data/Spotify_Youtube.csv'):
+        df = pd.read_csv(path, index_col=0)
+        self.db = {}
+        self.musicality_index = (df.columns.tolist().index("Danceability"), df.columns.tolist().index("Duration_ms"))
+        self.shape = df.shape
+        for index, row in df.iterrows():
+            self.add_song(row)
+
+    def add_song(self, row: pd.Series):
+        self.db[row.Artist] = self.db.get(row.Artist, []) + [Song(row)]
+
+    def display(self):
+        print(self.db)
+
+    def search(self, artist, n: int = 3):
+        songs = self.db.get(artist)
+        if songs is None:
+            return []
+        return sorted(songs, key=lambda song: song.likes, reverse=True)[:n]
+
+    def get_shape(self):
+        return self.shape
+
+    def find_similar_songs(self, song: str):
+        for element in sum(self.db.values(), []):
+            if not isinstance(element.title, float) and song in element.title:
+                print(element)
+    # TODO: return the closest song by search and then find similar by musicality
 songs = []
 LAST_INDEX = 24
-
-def read_data(path: str = 'data/Spotify_Youtube.csv'):
-    with open(path, 'r') as file:
-        header = file.readline().strip().split(',')
-        songs = [element for element in csv.reader(file)]
-    return songs, header
-
-
-def get_shape(songs: []):
-    return len(songs), len(songs[0])
-
 
 def get_min_max(songs: [], column: int):
     if column < 0 or column > len(songs[0]):
@@ -35,15 +71,19 @@ def get_average_likes(songs: []):
         song_map[i] =  likes_map[i] / song_map[i]
     return sorted(song_map.items(), key=lambda x: x[1], reverse=True)
 
+DB = Database()
+# DB.display()display
+# print(DB.search("SICK LEGEND"))
+# print(DB.get_shape())
+# DB.display()
+DB.find_similar_songs("Feel Good")
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--file')
-    args = parser.parse_args()
-
-    songs, header = read_data(args.file)
-    # print(songs)
-    # print(header)
-    print(get_min_max(songs, 9))
-    print(get_shape(songs))
-    print(get_average_likes(songs))
+# if __name__ == "__main__":
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--file')
+    # args = parser.parse_args()
+    #
+    # songs, header = read_data(args.file)
+    # print(get_min_max(songs, 9))
+    # print(get_shape(songs))
+    # print(get_average_likes(songs))
