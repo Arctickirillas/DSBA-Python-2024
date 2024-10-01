411e0028583015d9ab152a68bda3f1d68cfc158f
Matto Yuzuru <matveyryabushkin@gmail.com>
2024-10-01 12:23:50 +0300

    Added parsing of sim arts and arts bio

--- parser.py
+++ parser.py
@@ -2,9 +2,10 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-def parse_genre_by_artist(artist: str):
-    URL = "https://www.last.fm/music/{}"
+URL = "https://www.last.fm/music/{}"
 
+
+def parse_genre_by_artist(artist: str):
     response = requests.get(URL.format(artist)) 
 
     if response.status_code != 200:
@@ -23,5 +24,37 @@ def parse_genre_by_artist(artist: str):
     return tags
 
 
+def parse_similar_artists(artist: str):
+    response = requests.get(URL.format(artist) + '/+similar') 
+
+    if response.status_code != 200:
+        return []
+
+    soup = bs(response.text, 'html.parser')
+
+    catologue = soup.find('ol', class_='similar-artists')
+
+    try:
+        similar_artists = [el.text.strip() for el in catologue.find_all('h3')]
+    except:
+        return []
+
+    return similar_artists
+
+
+def parse_artist_bio(artist: str):
+    response = requests.get(URL.format(artist) + '/+wiki') 
+
+    if response.status_code != 200:
+        return []
+
+    soup = bs(response.text, 'html.parser')
+
+    wiki = soup.find('div', class_='wiki-content')
+    bio = [el.text.strip() for el in wiki.find_all('p')]
+    
+    return bio
+
+
 artist = input()
-print(parse_genre_by_artist(artist))
+print(parse_artist_bio(artist)[0])
